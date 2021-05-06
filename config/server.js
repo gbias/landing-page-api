8b6704d15ded7905899b8e9072a38627dd09274c
gbias <george.bias@outlook.com.br>
2021-05-06 01:44:59 -0300

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7e6ba54905677711b9f40e94854211e7'),
+    },
+  },
+});
